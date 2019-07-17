c6c419ab3b17e078cdc225db1876a5411ccc9339
Gary Preston <gary@mups.co.uk>
2019-07-17 23:36:11 +0100

    Move repos into json
    Enable repo updating without modification to scripts requiring admin accept modified script.

--- zathras.groovy
+++ zathras.groovy
@@ -5,76 +5,19 @@
 // Note: CI only supports repos on github currently.
 // Note2: Credentials and git url case must match the github case as the webhook
 //        is case sensitive.
+import groovy.json.JsonSlurper
 
-// TODO: read from a workspace repos.json to avoid script reauth on changes.
-//       Include credentials ID here when required
 // TODO: Remove owner/name and add projectUrl and base folder name?
 //       Once loading from json, sanity check that no repos use same base name.
 // TODO: seed jobs assume "master" branch is used for development which is true of
 //       replay_ repos but will want to be configurable to account for other
 //       repos using develop
-def repos = [
-  //
-  // Production Server
-  //
-  [
-    owner: 'Takasa',
-    name: 'replay_common',
-    url: 'git@github.com:Takasa/replay_common.git',
-    credentialId: 'takasa_replay_common'
-  ],
-  [
-    owner: 'Takasa',
-    name: 'replay_console',
-    url: 'git@github.com:Takasa/replay_console.git',
-    credentialId: 'takasa_replay_console'
-  ],
-  [
-    owner: 'Takasa',
-    name: 'replay_arcade',
-    url: 'git@github.com:Takasa/replay_arcade.git',
-    credentialId: 'takasa_replay_arcade'
-  ],
-  [
-    owner: 'Takasa',
-    name: 'replay_computer',
-    url: 'git@github.com:Takasa/replay_computer.git',
-    credentialId: 'takasa_replay_computer'
-  ],
-
-  //
-  // Test Server
-  //
-  [
-    owner: 'Sector14',
-    name: 'replay_common',
-    url: 'git@github.com:Sector14/replay_common.git',
-    credentialId: 'sector14_replay_common',
-    testingOnly: true
-  ],
-
-  [
-    owner: 'Sector14',
-    name: 'replay_arcade',
-    url: 'git@github.com:Sector14/replay_arcade.git',
-    credentialId: 'sector14_replay_arcade',
-    testingOnly: true
-  ],
-
-  // TODO: Add support for single core repo or rearrange repo to support sub dir
-  // [
-  //   owner: 'Sector14',
-  //   name: 'acorn_electron',
-  //   url: 'https://github.com/Sector14/acorn-electron-core.git'
-  // ],
-]
-
+// TODO: Support not monolythic repos without requiring subdir + _cores.txt
 
 // -----------------------------------------------------------------------------
 // Globals
 // -----------------------------------------------------------------------------
 
-// Wrap environment vars
 def configuration = new HashMap()
 def binding = getBinding()
 configuration.putAll(binding.getVariables())
@@ -86,59 +29,70 @@ Boolean isProduction = configuration['PRODUCTION_SERVER'] ? configuration['PRODU
 // -----------------------------------------------------------------------------
 out.println("Running on " + (isProduction ? "PRODUCTION" : "TEST") + " server.")
 
-folder('seed_jobs')
+def jsonSlurper = new JsonSlurper()
+def repo_list = jsonSlurper.parseText(readFileFromWorkspace('repos.json'))
 
-String seed_script = readFileFromWorkspace('seed_job.groovy')
+generateSeedJobs(repo_list, isProduction)
 
-repos.each { repo ->
-  if (repo.testingOnly && isProduction)
-    return
+// -----------------------------------------------------------------------------
+// Methods
+// -----------------------------------------------------------------------------
 
-  String job_name = "seed_jobs/${repo.owner}-${repo.name}-seeder"
+def generateSeedJobs(repos, isProduction) {
+  folder('seed_jobs')
 
-  job(job_name) {
-    description("Seed job for ${repo.url}.")
-    parameters {
-      // REVIEW: Don't really want these as ui editable params, better option?
-      stringParam('param_repo_owner', repo.owner, 'Do NOT modify')
-      stringParam('param_repo_name', repo.name, 'Do NOT modify')
-      credentialsParam('param_repo_credential_id') {
-        description('Do NOT modify')
-        defaultValue(repo.credentialId)
-      }
-      stringParam('param_repo_url', repo.url, 'Do Not modify')
-    }
-    // TODO: Switch to multiscm with replay_ci as extra repo
-    scm {
-      git {
-        remote {
-          url(repo.url)
-          credentials(repo.credentialId)
+  String seed_script = readFileFromWorkspace('seed_job.groovy')
+
+  repos.each { repo ->
+    if ( (repo.testingOnly && isProduction) || repo.disabled)
+      return
+
+    String job_name = "seed_jobs/${repo.owner}-${repo.name}-seeder"
+
+    job(job_name) {
+      description("Seed job for ${repo.url}.")
+      parameters {
+        // REVIEW: Don't really want these as ui editable params, better option?
+        stringParam('param_repo_owner', repo.owner, 'Do NOT modify')
+        stringParam('param_repo_name', repo.name, 'Do NOT modify')
+        credentialsParam('param_repo_credential_id') {
+          description('Do NOT modify')
+          defaultValue(repo.credentialId)
         }
-        branch('master')
-        extensions {
-          pathRestriction {
-            includedRegions('_cores.txt')
-            excludedRegions('')
+        stringParam('param_repo_url', repo.url, 'Do Not modify')
+      }
+      // TODO: Switch to multiscm with replay_ci as extra repo
+      scm {
+        git {
+          remote {
+            url(repo.url)
+            credentials(repo.credentialId)
+          }
+          branch('master')
+          extensions {
+            pathRestriction {
+              includedRegions('_cores.txt')
+              excludedRegions('')
+            }
           }
         }
       }
-    }
-    triggers {
-      gitHubPushTrigger()
-    }
-    steps {
-      jobDsl {
-        scriptText(seed_script)
-        removedJobAction('DELETE')
-        removedViewAction('DELETE')
-        removedConfigFilesAction('DELETE')
+      triggers {
+        gitHubPushTrigger()
+      }
+      steps {
+        jobDsl {
+          scriptText(seed_script)
+          removedJobAction('DELETE')
+          removedViewAction('DELETE')
+          removedConfigFilesAction('DELETE')
+        }
+      }
+      logRotator {
+        numToKeep(20)
       }
     }
-    logRotator {
-      numToKeep(20)
-    }
-  }
 
-  queue(job_name)
+    queue(job_name)
+  }
 }
