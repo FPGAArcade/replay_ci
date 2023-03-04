05ac971c385d1c38bca6e23cd4576741ec599e79
Gary Preston <gary@mups.co.uk>
2023-03-04 20:15:31 +0000

    Use new auth header Upload builds for devel train only.

--- scripts/publish_core.sh
+++ scripts/publish_core.sh
@@ -15,34 +15,37 @@ fi
 
 # DEPRECATED: Provides access to cores via raw directory access via web server. Left as a fallback
 #             for manual core downloads in case of future api issues.
-# Update "latest" sym link
 RELEASE_DIR="/home/jenkins/www/releases/builds/${RELEASE_TRAIN}/cores/${core_target}/${core_name}"
 mkdir -p "${RELEASE_DIR}"
 cp "${RELEASE_ZIP}" "${RELEASE_DIR}/"
 ln -sf "${RELEASE_DIR}/${RELEASE_ZIP_NAME}" "${RELEASE_DIR}/latest"
 
-echo "Uploading build ${BUILD_NUMBER} to '${RELEASE_TRAIN}' release train: ${RELEASE_ZIP}"
+# Only devel builds are uploaded now. Stable and other trains are via releaseTrain api only.
+if [ "${RELEASE_TRAIN}" = "devel" ]; then
+  echo "Uploading build ${BUILD_NUMBER} to '${RELEASE_TRAIN}' release train: ${RELEASE_ZIP}"
 
-# TODO: Once promotion endpoint available, only upload build for "devel" train
-# Upload to release api
-status=`curl --silent --output /dev/stderr -w "%{http_code}" --request POST \
-            --header "Authorization: APIKey ${releaseapikey}" \
-            --form "buildinfo={
-                        \"platformId\": \"${core_target}\",
-                        \"coreId\": \"${core_name}\",
-                        \"buildType\": \"core\",
-                        \"releaseTrain\": \"${RELEASE_TRAIN}\",
-                        \"buildDate\": \"${BUILD_TIMESTAMP}\"
-                      };type=application/json" \
-            --form "zipfile=@\"${RELEASE_ZIP}\";type=application/zip" \
-            ${RELEASE_API_URL}builds/`
+  # Upload to release api
+  status=`curl --silent --output /dev/stderr -w "%{http_code}" --request POST \
+              --header "x-api-key: ${releaseapikey}" \
+              --form "buildinfo={
+                          \"platformId\": \"${core_target}\",
+                          \"coreId\": \"${core_name}\",
+                          \"buildType\": \"core\",
+                          \"releaseTrain\": \"${RELEASE_TRAIN}\",
+                          \"buildDate\": \"${BUILD_TIMESTAMP}\"
+                        };type=application/json" \
+              --form "zipfile=@\"${RELEASE_ZIP}\";type=application/zip" \
+              ${RELEASE_API_URL}builds/`
 
-if [ "${status}" -lt 200 ] || [ "${status}" -ge 300 ]; then
-  echo >&2 "API upload failed. Aborting."
-  exit 1
+  if [ "${status}" -lt 200 ] || [ "${status}" -ge 300 ]; then
+    echo >&2 "API upload failed. Aborting."
+    exit 1
+  fi
+
 fi
 
 # TODO: Remove and perform notification as part of backend api for stable releases.
+#       Promotion will be removed entirely from Jenkins once auth'd web frontend available
 # Notify discord
 if [ "${RELEASE_TRAIN}" = "stable" ]; then
 read -d '' DISCORD_MESSAGE <<EOF
